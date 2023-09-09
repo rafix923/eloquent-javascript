9a3d4ce6ad636f173446577f24e9a5fd46d03389
Rafael Ramos de Oliveira Lopes <rafaellopes.dev@gmail.com>
2023-09-09 02:39:40 -0300

    feat: Aplicar o laço "for" para gerar como saída a estrutura de um triângulo como no exemplo do enunciado da questão.
    A cada interação, a nova linha deverá receber mais um "#", possuindo no final um total de 7 linhas.

---
+++ looping-a-triangle.js
@@ -0,0 +1,12 @@
+// Write a loop that makes seven calls to console.log to output the following triangle:
+// #
+// ##
+// ###
+// ####
+// #####
+// ######
+// #######
+
+for (let i = "#"; i.length <= 7; i += "#") {
+    console.log(i);
+}
